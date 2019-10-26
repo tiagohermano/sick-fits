beda9b734cb116e2571a7d8f79ca070f52ba6611
Tiago Hermano <tiagofhermano@gmail.com>
2019-10-26 15:38:43 -0300

    Update Item done

---
+++ frontend/pages/update.js
@@ -0,0 +1,11 @@
+import UpdateItem from "../components/UpdateItem";
+
+const Sell = (props) => {
+  return (
+    <div>
+      <UpdateItem id={props.query.id} />
+    </div>
+  );
+}
+
+export default Sell;
