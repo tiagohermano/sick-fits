ddb3480fb78e3408945bebf328255c814a9fe162
Tiago Hermano <tiagofhermano@gmail.com>
2019-10-27 17:32:52 -0300

    backend createUser mutation and middleware logic

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if the user is logged in
@@ -27,6 +30,25 @@ const Mutation = {
     // TODO
     // 3. delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+    // create the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+    return user;
   }
 };
 
